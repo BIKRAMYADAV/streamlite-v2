dd566f668526e6eb95df54b0d1cf4f677ba1d292
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-01-29 19:36:49 +0530

    feat: sets up ws server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const PORT = process.env.PORT 
-const express = require('express')
-const app = express()
 
-app.listen(PORT, () => {
-    console.log(`The server is listening on port ${PORT}`)
-})
+const ws = require('ws')
+const wss = new ws.WebSocketServer({port:8080})
+
+wss.on('error', console.error);
+
+wss.on('connection', (ws) => {
+    console.log('a new client connected')
+    ws.on('error', console.error); 
+    ws.on('message', () => console.log('message') );
+    ws.on('close', () => console.log('close'));
+} )
+
+
