f324acc1f0ce8e07c676e9401080a969d18b1bfe
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-01-29 20:25:00 +0530

    feat: implements create room logic in ws

--- server/index.js
+++ server/index.js
@@ -2,15 +2,38 @@ const dotenv = require('dotenv')
 dotenv.config()
 const PORT = process.env.PORT 
 
+const {generateUUID} = require('uuid')
+// const uid = uuid();
+
 const ws = require('ws')
 const wss = new ws.WebSocketServer({port:8080})
+
+//rooms object
+const rooms = {};
 
 wss.on('error', console.error);
 
 wss.on('connection', (ws) => {
     console.log('a new client connected')
     ws.on('error', console.error); 
-    ws.on('message', () => console.log('message') );
+    ws.on('message', (msg) =>{
+       const parsedmsg = JSON.parse(msg);
+       if(parsedmsg.type === "create"){
+        const roomId = generateUUID();
+        rooms[roomId] = {
+            host: ws,
+            viewer: null 
+        }
+        ws.roomId = roomId
+        ws.role = "host"
+        ws.send(JSON.stringify({
+            "type" : "room-created",
+            "roomId" : roomId
+        }))
+       }else if(parsedmsg === "join"){
+
+       }
+    });
     ws.on('close', () => console.log('close'));
 } )
 
